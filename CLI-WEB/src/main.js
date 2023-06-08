91ec6c860c787567372f0a6f627844eb72ea839f
Gastón Nicora <gastonmatias_21@hotmail.com>
2023-06-08 15:45:41 -0300

    var

--- CLI-WEB/src/main.js
+++ CLI-WEB/src/main.js
@@ -1,6 +1,14 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './assets/main.css'
+
+import router from './router'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap'
+
+
+
+createApp(App).use(router).mount('#app')
