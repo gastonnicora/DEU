ab0230b1117fd6ee44c92630cd72a02eb7a6df61
lzarate <zaratelucianoa@gmail.com>
2023-06-08 01:20:25 -0300

    axios

--- CLI-WEB/src/main.js
+++ CLI-WEB/src/main.js
@@ -2,5 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueAxios, axios) 
+app.mount('#app')
