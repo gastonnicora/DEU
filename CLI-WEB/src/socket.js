83e87cd2432f2d39d14e90542531ee15aef88a56
NICORA, GASTON MATIAS <gastonmatias_21@hotmail.com>
2023-08-05 19:39:43 -0300

    pruba socket

--- CLI-WEB/src/socket.js
+++ CLI-WEB/src/socket.js
@@ -11,7 +11,7 @@ if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
     URL = "http://localhost:4000/"
 }
 else {
-    URL = "api/"
+    URL = "api"
 }
 
 export const socket = io(URL);
